9082456ecee4cdf3fe89897199ac43a7f1700f67
Kiptoo Dennis <denysmutay@gmail.com>
2022-05-03 16:54:32 +0600

    update to view function for registration

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
-from app import app
-from flask import render_template, redirect
+from app import app, db
+from flask import render_template, redirect, url_for, flash
+from app.models import userdatastore, security
+from app.forms import RegistrationForm
+from app.models import User
 
 
 @app.route('/')
@@ -8,8 +11,17 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
+@app.before_first_request
 def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        userdatastore.create_user(lastname=form.lastname.data, firstname=form.firstname.data, email=form.email.data,
+                                  password=form.password.data)
+        db.session.add(userdatastore)
+        db.session.commit()
+        return 'Created account'
+    flash('Your account has been created!', 'success')
     return render_template('register.html')
 
 

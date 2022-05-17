b992a8430c09877c3bcd0c6a0dba4cb4dd446115
kiptoo <denysmutay@gmail.com>
2022-05-17 19:20:04 +0300

    hashed passwords using weukzeug

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 # from sqlalchemy import ForeignKey
 
-from app import db, app
+from app import db
 from flask_security import UserMixin, RoleMixin, SQLAlchemyUserDatastore, Security
 from datetime import date, datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 roles_users = db.Table('roles_users',
                        db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
@@ -14,13 +15,19 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     location = db.Column(db.String(255))
     active = db.Column(db.Boolean(), default=True)
     created_on = db.Column(db.DateTime, default=date.today())
     # roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
     status=db.Column(db.String(55),default='normal')
     products=db.relationship('Product',backref='dealer',lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Role(db.Model, RoleMixin):

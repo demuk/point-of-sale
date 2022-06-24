033381ec4b250e2596081d220c203568b421a85e
kiptoo <denysmutay@gmail.com>
2022-06-25 00:56:17 +0300

    updated URI

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-    	'postgres://', 'postgresql://') or \
+    SQLALCHEMY_DATABASE_URI = 'postgres://bnhxyunkwohjxf:edfa41baf5f188be4ca7ef8e78f2fd2d3a53003590a1e5bb92a31e688f1c1165@ec2-23-23-182-238.compute-1.amazonaws.com:5432/d5jlnsibam9772' \
         'sqlite:///' + os.path.join(basedir, 'pos.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'kjfdkjfkdjfkdjfkdkdklfjlksjdfkldjfkldf'

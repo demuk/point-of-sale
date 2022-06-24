b29b473e24aaf25a0165fdab3524fe8beea16aff
kiptoo <denysmutay@gmail.com>
2022-06-24 23:06:27 +0300

    heroku deployment changes

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'pos.db')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+    	'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'pos.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'kjfdkjfkdjfkdjfkdkdklfjlksjdfkldjfkldf'
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')

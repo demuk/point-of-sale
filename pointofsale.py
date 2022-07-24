e126448a6d4c0dcf19461809b259364aaff6d080
Kiptoo Dennis <denysmutay@gmail.com>
2022-07-25 00:08:34 +0300

    added browser startup

--- pointofsale.py
+++ pointofsale.py
@@ -1,1 +1,12 @@
+import webbrowser
+import threading
+import random
+
+
 from app import app
+
+if __name__ == '__main__':
+    port = 5000 + random.randint(0, 999)
+    url = "http://127.0.0.1:{0}".format(port)
+    threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+    app.run(port=port, debug=False)
